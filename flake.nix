56af191f8846fb15cc33e9cf928946ee9f3bd086
Shaleen Jain <shaleen@jain.sh>
2025-02-07 12:38:09 +0530

    emanote: try again

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
       imports = [ inputs.emanote.flakeModule ];
       perSystem = { self', pkgs, system, ... }: {
         emanote.sites."shalzz" = {
-          layers = [{ pathString = "./"; }];
+          layers = [{ path = / ; pathString = "/"; }];
           port = 9801;
           prettyUrls = true;
         };
