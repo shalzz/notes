ca22e4ae434b41260a459a5899c2e992eaf90708
Shaleen Jain <shaleen@jain.sh>
2025-02-07 12:34:25 +0530

    emanote: try again

--- flake.nix
+++ flake.nix
@@ -1,40 +1,36 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    emanote.url = "github:srid/emanote";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+  description = "Shaleen's Personal Webside";
+  nixConfig = {
+    extra-substituters = "https://cache.garnix.io";
+    extra-trusted-public-keys = "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=";
   };
 
-  outputs =
-    inputs@{
-      self,
-      nixpkgs,
-      flake-parts,
-      ...
-    }:
+  inputs = {
+    emanote.url = "github:srid/emanote";
+    nixpkgs.follows = "emanote/nixpkgs";
+    flake-parts.follows = "emanote/flake-parts";
+  };
+
+  outputs = inputs@{ self, flake-parts, nixpkgs, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
-
-      imports = [
-        inputs.emanote.flakeModule
-      ];
-
-      perSystem =
-        { config, pkgs, ... }:
-        {
-          emanote = {
-            sites = {
-              default = {
-                layers = [
-                  {
-                    path = ./content;
-                    pathString = "./content";
-                  }
-                ];
-                prettyUrls = true;
-              };
-            };
-          };
+      imports = [ inputs.emanote.flakeModule ];
+      perSystem = { self', pkgs, system, ... }: {
+        emanote.sites."shalzz" = {
+          layers = [{ path = ./ ; pathString = "./"; }];
+          port = 9801;
+          prettyUrls = true;
         };
+        apps.default.program = self'.apps.shalzz.program;
+        packages.default = pkgs.symlinkJoin {
+          name = "shalzz-static-site";
+          paths = [ self'.packages.shalzz ];
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nixpkgs-fmt
+          ];
+        };
+      };
     };
 }
